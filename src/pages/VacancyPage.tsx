997dacb6a8f67a81c886818f89c54edd217160b8
Иван Синев <ie.sinev.work@gmail.com>
2026-06-03 07:51:04 +0300

    refresh page fix

--- src/pages/VacancyPage.tsx
+++ src/pages/VacancyPage.tsx
@@ -1,18 +1,31 @@
-import type { RootState } from '../store/store';
-import { useSelector } from "react-redux";
+import { type AppDispatch, type RootState } from '../store/store';
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Header from '../components/Header/Header';
 import VacancyCard from '../components/VacancyCard/VacancyCard';
 import { Flex, Container, Box } from '@mantine/core';
 import CompanyInfo from '../components/CompanyInfo/CompanyInfo';
+import { useEffect } from 'react';
+import { fetchVacancies } from '../store/vacanciesSlice';
 
 function VacancyPage() {
-
+    const dispatch = useDispatch<AppDispatch>()
     const { id } = useParams()
 
     const vacancies = useSelector((state: RootState) => state.vacancies.vacancies)
-    const vacancy = vacancies.find(vac => vac.id === id)
+    
 
+    useEffect(() => {
+        if (vacancies.length === 0) {
+            dispatch(fetchVacancies())
+        }
+    }, [dispatch, vacancies.length])
+    
+    const vacancy = vacancies.find(vac => vac.id === id)
+    if (vacancies.length === 0) {
+        return <div>Загрузка</div>
+    }
+    
     if (!vacancy) {
         throw new Error('Вакансия не найдена')
     }
